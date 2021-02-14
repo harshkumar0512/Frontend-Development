293fb84bda9cbabe84e6b39d141b33b30167094d
harshkumar0512 <55206284+harshkumar0512@users.noreply.github.com>
2021-02-15 00:21:44 +0530

    Post page UI design

--- scripts/post.js
+++ scripts/post.js
@@ -16,28 +16,49 @@
 				modal.style.display = "none";
 			}
 		}
-		// Get the modal
-		var modal = document.getElementById('delete01');
+		function editPost(){
+			const paragraph = document.getElementById("paraText");
+			const title = document.getElementById("postTitle01");
+			const edit_button = document.getElementById("edit-button");
+			const end_button = document.getElementById("end-editing");
 
-		// When the user clicks anywhere outside of the modal, close it
-		window.onclick = function(event) {
-		  if (event.target == modal) {
-			modal.style.display = "none";
-		  }
+			edit_button.addEventListener("click", function() {
+				paragraph.contentEditable = true;
+				paragraph.style.backgroundColor = "#dddbdb";
+			} );
+
+			end_button.addEventListener("click", function() {
+				paragraph.contentEditable = false;
+				paragraph.style.backgroundColor = "#ffe44d";
+			} )
+
+			var editButton = listItem.querySelector("button.edit");
+
+			if(containsClass) {
+				label.innerText = editInput.value;
+				editButton.textContent = "Edit";
+			} else {
+				editInput.value = label.innerText;
+				editButton.textContent = "Save";
+			}
+
 		}
-
-		//Declare post id
-		var id = null;
-
-		//deletePosts function will take post id and delete that post.
-		function deletePosts(id){
-			console.log("This method is called");
-			var modal = document.getElementById('delete01');
-			modal.style.display = "none";
-			// call the remaining post to display
-
+		function addComments(id){
+			var addEventName = id.value +'<br>';
+			var a = document.getElementById('userComments');
+			document.getElementById('addEventNames').innerHTML += '<p>'+addEventName+'</p>';
+			a.value=a.defaultValue;
 		}
-
-		function navigateToPost(){
-			location.replace('../html/post.html');
+		var numberOfClicks=0;
+		function postLiked(){
+			document.getElementById("likePost").innerHTML = "<i class='fa fa-thumbs-up'></i> Liked";
+			numberOfClicks += 1;
+			if(numberOfClicks!=0){
+				if (numberOfClicks==1){
+					document.getElementById('commentCount').innerHTML = numberOfClicks + " person likes this!";
+				}
+				else {
+					document.getElementById('commentCount').innerHTML = numberOfClicks + " people like this!";
+				}
+			}
 		}
