f4954ef50cb2ec8be7c28984c9acfe1fe4f5bf91
harshkumar0512 <55206284+harshkumar0512@users.noreply.github.com>
2021-02-15 22:58:15 +0530

    minor UI style changes

--- scripts/postslist.js
+++ scripts/postslist.js
@@ -25,19 +25,31 @@
 			modal.style.display = "none";
 		  }
 		}
+		function ConfirmationModal() {
+			console.log('This method is executed');
+			var modal = document.getElementById('delete01');
+			modal.style.display = "block";
+			var link = document.querySelector('.post-card');
+			if(link){
+				id = link.getAttribute('id');
+				console.log(id);
+			}
+		}
 
 		//Declare post id
 		var id = null;
 
 		//deletePosts function will take post id and delete that post.
-		function deletePosts(id){
-			console.log("This method is called");
+		function deletePosts(){
+
 			var modal = document.getElementById('delete01');
 			modal.style.display = "none";
 			// call the remaining post to display
-
+			document.getElementById('post05').innerHTML = '';
 		}
 
 		function navigateToPost(){
 			location.replace('../html/post.html');
 		}
+
+
